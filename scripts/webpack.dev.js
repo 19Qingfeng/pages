007acdb62112e48730bea59f3d023963f6f3b6ea
wang.haoyu <228233464@qq.com>
2021-09-23 19:32:43 +0800

    feat: dev-server

---
+++ scripts/webpack.dev.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base');
+const path = require('path');
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    // static允许我们在DevServer下访问该目录的静态资源
+    // 简单理解来说 当我们启动DevServer时相当于启动了一个本地服务器
+    // 这个服务器会同时以static-directory目录作为跟路径启动
+    // 这样的话就可以访问到static/directory下的资源了
+    static: {
+      directory: path.join(__dirname, '../public'),
+    },
+    // 默认为true
+    hot: true,
+    // 是否开启代码压缩
+    compress: true,
+    // 启动的端口
+    port: 9000,
+  },
+};
+
+module.exports = merge(devConfig, baseConfig);
